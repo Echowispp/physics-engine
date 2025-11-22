89658c0fbf91bd4aeabc6b6e32b0621c49c90d0a
Echowispp <kauri.sykari190@gmail.com>
2025-11-22 15:21:46 +0200

    added system for drawing circles on canvas
    also did some logic on adding the shapes to mesh

--- main.js
+++ main.js
@@ -1,25 +1,72 @@
-let c = document.getElementById("canvas");
-let ctx = c.getContext("2d");
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+let selectedShape = null;
+let mouseX = 0;
+let mouseY = 0;
 
-function onButtonPressed() {
-    ctx.fillStyle = "#a20";
-    ctx.fillRect(20, 20, 75, 50);
+window.addEventListener("load", function () {
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+});
+
+function onCircButtonPressed() {
+    selectedShape = "circ";
+    console.log(mesh);
 }
 
+canvas.addEventListener("click", function (event) {
+    if (!selectedShape) {
+        return;
+    }
+    const rect = canvas.getBoundingClientRect();
+    mouseX = event.clientX - rect.left;
+    mouseY = event.clientY - rect.top;
+
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#a20";
+
+    if (selectedShape == "circ") {
+        var radius = 40;
+        var letter = "A";
+
+        if (mesh.objA.defined == true) {
+            letter = "B";
+        }
+        if (mesh.objB.defined == true) {
+            return;
+        }
+
+        ctx.beginPath();
+
+        ctx.arc(mouseX, mouseY, radius, 0, 2 * Math.PI);
+        ctx.fill();
+
+        let obj = mesh["obj" + letter];
+        if (!obj.defined) {
+            obj.pos = [mouseX, mouseY];
+            obj.size = radius;
+            obj.objId = "circ";
+            obj.defined = true;
+        }
+        console.log(obj);
+    }
+});
+
 let mesh = {
-    /*objA: {
-        pos: [x, y],
-        size: r,
+    objA: {
+        pos: [0, 0],
+        size: 0,
         objId: "",
+        defined: false,
     },
     objB: {
-        pos: [x, y],
-        size: r,
+        pos: [0, 0],
+        size: 0,
         objId: "",
-    },*/
+    },
 
     doCollisions() {
-        /*switch (this.objA.objId + ":" + this.objB.objId) {
+        switch (this.objA.objId + ":" + this.objB.objId) {
             case "circ:circ": {
                 this.CC(this.objA, this.objB);
                 break;
@@ -34,27 +81,30 @@ let mesh = {
                 this.CS(this.objB, this.objA);
                 break;
             }
-                
+
             case "rect:rect": {
                 this.SS(this.objA, this.objB);
                 break;
             }
-        }*/
+        }
     },
 
+    // ===================
     // COLLISION FUNCTIONS
+    // ===================
 
-    /*CS(a, b) {
+    CS(a, b) {
         return;
     },
 
     CC(a, b) {
+        console.log("CC called!");
         return;
     },
 
     SS(a, b) {
         return;
-    },*/
+    },
 
     /*isInside(edges, xp, yp) {
         let cnt = 0;
